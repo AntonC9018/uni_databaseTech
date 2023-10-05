72b2dfb172806705ff9979ced91c04e960b875e5
AntonC9018 <anton.curm@gmail.com>
2023-10-05 19:55:44 +0300

    Initial

---
+++ Curs1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false)
+    .Build();
+
+var connectionString = config.GetValue<string>("ConnectionStrings:MainDatabase");
+using var connection = new SqlConnection(connectionString);
+connection.Open();
+{
+    using var command = connection.CreateCommand();
+    command.CommandText = "SELECT * FROM Region";
+    using var reader = command.ExecuteReader();
+    foreach (var value in reader)
+    {
+        Console.WriteLine(value);
+    }
+}
+connection.Close();
